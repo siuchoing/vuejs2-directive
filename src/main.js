5c18d7087d158e2c6b5e4ae4705cb3014bfcf611
altantic <15200582@life.hlkbu.edu.hk>
2020-01-19 22:48:39 +0800

    passing values to highlight directive

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import App from './App.vue'
 // create a global v-directive called highlight, use by v-highlight to an element.
 Vue.directive('highlight', {
   bind(el, binding, vnode) {  // one directive is attached
-    el.style.backgroundColor = 'green';
+    //el.style.backgroundColor = 'green';
+    el.style.backgroundColor = binding.value;
   }
 });
 
