41dd312eac4ae2332a9326c144d61db3bcc13b5d
altantic <15200582@life.hlkbu.edu.hk>
2020-01-19 22:43:56 +0800

    binding style backgroundColor by highlight global directive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// create a global v-directive called highlight, use by v-highlight to an element.
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {  // one directive is attached
+    el.style.backgroundColor = 'green';
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
